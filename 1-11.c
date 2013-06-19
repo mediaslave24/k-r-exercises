50bbab1b54dec845930fa2850566846329f7b2af
Michael <mmaccoffe@gmail.com>
2013-06-19 23:23:13 +0300

    1-11

---
+++ 1-11.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define IN 10
+#define OUT 11
+
+main()
+{
+  char c, state;
+
+  state = OUT;
+  while((c = getchar()) != EOF)
+    if ( state == IN && ( c == ' ' || c == '\t') ) {
+      state = OUT; 
+      putchar('\n');
+    }
+    else {
+      state = IN; 
+      putchar(c);
+    }
+  return 0;
+}
